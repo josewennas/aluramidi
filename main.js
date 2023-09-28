5c8b84a408b684b6e0117ea2b3cc8933ba371d84
José Wennas <josewennas@hotmail.com>
2023-09-28 13:41:53 -0300

    aula 05/02

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-document.querySelector('.tecla_pom');
+// ///////////SELECIONA APENAS UM BOTÃO PARA SER CLICADO
+// function tocaSomPom() {
+//     document.querySelector('#som_tecla_pom').play();
+// }
+
+// document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+ function tocaSomPom() {
+     document.querySelector('#som_tecla_pom').play();
+ }
+
+ document.querySelectorAll('tecla');
